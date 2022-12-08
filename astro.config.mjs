cf478b8ccf178d2d1646254b39cc59c1b979843c
sinhnticts <sinhnt@icts.vn>
2022-12-08 10:28:51 +0700

    feat: add path alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,24 @@
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
+import path from "node:path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress()],
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: "@styles",
+          replacement: path.resolve(__dirname, "./src/styles"),
+        },
+      ],
+    },
+  },
 });
